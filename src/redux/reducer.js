8900443bbbc3c99679925a73f69fedcf38af5ea3
Nahid2194 <nahidjc80@gmail.com>
2020-11-16 09:02:28 +0600

    adding redux

---
+++ src/redux/reducer.js
@@ -0,0 +1,10 @@
+import DISHES from '../data/dishes';
+import COMMENTS from '../data/comments';
+const initState = {
+    dishes: DISHES,
+    comments: COMMENTS
+}
+
+export const Reducer = (state = initState, action) => {
+    return state;
+}
